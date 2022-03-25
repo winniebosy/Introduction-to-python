d219712127b0712f99b8d168d1e8e891bdc4e45b
Winnie Bosibori <winniebosy@gmail.com>
2022-03-25 15:25:21 +0300

    testing python

---
+++ test.py
@@ -0,0 +1,9 @@
+
+message = input("What is your char")
+
+for ch in message:
+    # Add your code here.
+    if ch =='?':
+        print('snese much curiosity in you, i do')
+    else:
+        print('Enthusiastic, you are')
