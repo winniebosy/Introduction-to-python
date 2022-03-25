c2bc498b386e54cf339d8c09ffc52c4fe1ffd4e9
Winnie Bosibori <winniebosy@gmail.com>
2022-03-25 12:49:55 +0300

    notes and python examples

--- greeting.py
+++ greeting.py
@@ -2,3 +2,6 @@ name = input("What's your name?")
 print("Hello,", name, "!")
 print("Nice to meet you!", name)
 print("hELLO WOORLD", name)
+
+n = int(input("Please enter an number"))
+print("tHIS IS A",n)
