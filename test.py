868476899d278b3c3aa335cca86694e748867f29
Winnie Bosibori <winniebosy@gmail.com>
2022-03-26 21:38:03 +0300

    notes

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 
+from doctest import OutputChecker
+
+
 message = input("What is your char")
 
 for ch in message:
@@ -33,3 +36,35 @@ length = len(word)
 for n in range(length):
     # Add your code here.
     print(word[:n+1])
+
+
+#write a function that will produce the below
+#abracadabra
+#abracadabr
+#abracadab
+#abracada
+#abracad
+#abraca
+#abrac
+#abra
+#abr
+#ab
+#a
+
+
+
+
+    def word_triangle(word):
+    # Add your code here
+    length= len(word)
+    for n in range(length):
+        print(word[:length-n])
+              
+word_triangle("abracadabra")
+
+#explanantion
+#the slice expression here is word[:length - n]. Since length stays the same while n grows, 
+# this means that on each pass through the loop, 
+# length - n gets smaller and smaller, from length down to 1. 
+# And that means that on each pass, the string that gets printed will be a shorter and shorter piece of word.
+
