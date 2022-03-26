b1e5af1ad53e3ec3c2f90f46f6419d7ee5cd2c3a
Winnie Bosibori <winniebosy@gmail.com>
2022-03-26 05:55:46 +0300

    slicing in python

--- test.py
+++ test.py
@@ -7,3 +7,29 @@ for ch in message:
         print('snese much curiosity in you, i do')
     else:
         print('Enthusiastic, you are')
+
+
+
+
+#EX EXAMPLE
+#Exercise: Word triangle
+# In the workspace below you'll find a partially completed for loop.
+
+#Your goal is to finish the loop so that it prints out the following:
+#d
+#de
+#def
+#defi
+#defin
+#defini
+#definit
+#definite
+#definitel
+#definitely
+
+word = "definitely"
+length = len(word)
+
+for n in range(length):
+    # Add your code here.
+    print(word[:n+1])
