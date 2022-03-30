f3e8ba8b224c8f62fb99b5cc0fd8637006fbfa7d
Winnie Bosibori <winniebosy@gmail.com>
2022-03-30 16:16:39 +0300

    breakfast bot

---
+++ t.py
@@ -0,0 +1,20 @@
+print("hello wot!")
+
+story = """
+            Once upon a time there was a very long string 
+            that was over 100 characters long and could not 
+            fit on the screen at once.
+           
+        """
+        
+story_One = "Once upon a time there was a very long string that was \
+            over 100 characters long and could not all fit on the \
+            screen at once."
+
+story_Two = ("Once upon a time there was a very long string that" 
+                "was over 100 characters long and could not all fit on the" 
+                "screen at once or twice or thrice.")
+        
+print(story)
+print (story_One) 
+print (story_Two)
