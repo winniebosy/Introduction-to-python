c44f93fb8e0e60fdcfcce91d5c994f652eaeaa89
Winnie Bosibori <winniebosy@gmail.com>
2022-03-31 08:59:19 +0300

    refactoring code with function definition

--- breakFast.py
+++ breakFast.py
@@ -9,44 +9,52 @@
 #displays the information before a user orders
 import time
 
-print("Hello! I am Bob, the Breakfast Bot.")
-time.sleep(2)
-print("Today we have two breakfasts available.")
-time.sleep(2)
-print("The first is waffles with strawberries and whipped cream.")
-time.sleep(2)
-print("The second is sweet potato pancakes with butter and syrup.")
-time.sleep(2)
+def print_pause(message_to_print):
+    print(message_to_print)
+    time.sleep(2)
+
+print_pause("Hello! I am Bob, the Breakfast Bot.")
+print_pause("Today we have two breakfasts available.")
+print_pause("The first is waffles with strawberries and whipped cream.")
+print_pause("The second is sweet potato pancakes with butter and syrup.")
+
         
 
-user_response =""
-
+while True:
 #loop, asking the user for input over and over
-while user_response != "waffles" and user_response != "pancakes" :
-        user_response = input("Please place your order. Would you like waffles or pancakes?\n").lower() 
+        while True:
+            user_response = input("Please place your order. Would you like waffles or pancakes?\n").lower() 
         
 #user selects waffles or pancakes
-        if "waffles" in user_response:
-            print("Waffles it is!")
-            break
+            if "waffles" in user_response:
+                print("Waffles it is!")
+                print("Your order will be ready shortly")
+                break
     
-        elif "pancakes" in user_response:
-            print("Pancakes it is!")
-            break
+            elif "pancakes" in user_response:
+                print("Pancakes it is!")
+                print("Your order will be ready shortly")
+                break
  #when response not available   
-        else:
-            print("""Sorry, Your order is currently not available.""")
+            else:
+                print("""Sorry, Your order is currently not available.""")
+            time.sleep(2)
+            
+        while True: 
 
-print("Your order will be ready shortly")
-time.sleep(2)
-
-second_order= input("Would you like to place another order? Please say 'yes' or 'no: \n").lower()
-
-if "no" in second_order:
-    print("Ok! Goodbye..See you next time")
-    time.sleep(2)
-     break
-
-elif 'yes' in second_order:
-    print("Very good, I'm happy to take another order.")
-time.sleep(2)
+          second_order= input("Would you like to place another order? Please say 'yes' or 'no: \n").lower()
+       
+          if "no" in second_order:
+                print("OK! Thank you.See you next time")
+                time.sleep(2)
+                break
+           
+          elif 'yes' in second_order:
+                print("Very good, I'm happy to take another order.")
+                time.sleep(2)
+          else:
+                print("I do not understand your order")
+                time.sleep(1)
+                break
+            #breaking the loop
+        
